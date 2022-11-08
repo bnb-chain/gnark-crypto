91e9ae1d35cf43121100c81f665f05edd5049fdd
ycen <yycen@live.com>
2022-11-08 17:18:00 +0800

    Buffered raw decoding

--- ecc/bn254/marshal.go
+++ ecc/bn254/marshal.go
@@ -19,14 +19,11 @@ package bn254
 import (
 	"encoding/binary"
 	"errors"
-	"io"
-	"reflect"
-	"sync/atomic"
-
 	"github.com/consensys/gnark-crypto/ecc/bn254/fp"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
 	"github.com/consensys/gnark-crypto/ecc/bn254/internal/fptower"
-	"github.com/consensys/gnark-crypto/internal/parallel"
+	"io"
+	"reflect"
 )
 
 // To encode G1Affine and G2Affine points, we mask the most significant bits with these bits to specify without ambiguity
@@ -39,6 +36,7 @@ const (
 	mCompressedSmallest byte = 0b10 << 6
 	mCompressedLargest  byte = 0b11 << 6
 	mCompressedInfinity byte = 0b01 << 6
+	bufSz               int  = 0b01 << 22
 )
 
 // SizeOfGT represents the size in bytes that a GT element need in binary form
@@ -192,51 +190,33 @@ func (dec *Decoder) Decode(v interface{}) (err error) {
 		if len(*t) != int(sliceLen) {
 			*t = make([]G1Affine, sliceLen)
 		}
-		compressed := make([]bool, sliceLen)
-		for i := 0; i < len(*t); i++ {
-
-			// we start by reading compressed point size, if metadata tells us it is uncompressed, we read more.
-			read, err = io.ReadFull(dec.r, buf[:SizeOfG1AffineCompressed])
+		remain := int(sliceLen)
+		offset := 0
+		var bufAll [SizeOfG1AffineUncompressed * bufSz]byte
+		for {
+			if remain == 0 {
+				break
+			}
+			toRead := bufSz
+			if toRead > remain {
+				toRead = remain
+			}
+			remain -= toRead
+			nbBytes := SizeOfG1AffineUncompressed * toRead
+			read, err = io.ReadFull(dec.r, bufAll[:nbBytes])
 			dec.n += int64(read)
 			if err != nil {
-				return
+				return err
 			}
-			nbBytes := SizeOfG1AffineCompressed
-			// most significant byte contains metadata
-			if !isCompressed(buf[0]) {
-				nbBytes = SizeOfG1AffineUncompressed
-				// we read more.
-				read, err = io.ReadFull(dec.r, buf[SizeOfG1AffineCompressed:SizeOfG1AffineUncompressed])
-				dec.n += int64(read)
+			for i := 0; i < toRead; i++ {
+				_, err = (*t)[i+offset].setBytes(bufAll[i*SizeOfG1AffineUncompressed:(i+1)*SizeOfG1AffineUncompressed], false)
 				if err != nil {
-					return
-				}
-				_, err = (*t)[i].setBytes(buf[:nbBytes], false)
-				if err != nil {
-					return
-				}
-			} else {
-				compressed[i] = !((*t)[i].unsafeSetCompressedBytes(buf[:nbBytes]))
-			}
-		}
-		var nbErrs uint64
-		parallel.Execute(len(compressed), func(start, end int) {
-			for i := start; i < end; i++ {
-				if compressed[i] {
-					if err := (*t)[i].unsafeComputeY(dec.subGroupCheck); err != nil {
-						atomic.AddUint64(&nbErrs, 1)
-					}
-				} else if dec.subGroupCheck {
-					if !(*t)[i].IsInSubGroup() {
-						atomic.AddUint64(&nbErrs, 1)
-					}
+					return err
 				}
 			}
-		})
-		if nbErrs != 0 {
-			return errors.New("point decompression failed")
-		}
+			offset += toRead
 
+		}
 		return nil
 	case *[]G2Affine:
 		var sliceLen uint32
@@ -247,51 +227,34 @@ func (dec *Decoder) Decode(v interface{}) (err error) {
 		if len(*t) != int(sliceLen) {
 			*t = make([]G2Affine, sliceLen)
 		}
-		compressed := make([]bool, sliceLen)
-		for i := 0; i < len(*t); i++ {
+		remain := int(sliceLen)
+		offset := 0
+		var bufAll [SizeOfG2AffineUncompressed * bufSz]byte
+		for {
+			if remain == 0 {
+				break
+			}
 
-			// we start by reading compressed point size, if metadata tells us it is uncompressed, we read more.
-			read, err = io.ReadFull(dec.r, buf[:SizeOfG2AffineCompressed])
+			toRead := bufSz
+			if toRead > remain {
+				toRead = remain
+			}
+			remain -= toRead
+			nbBytes := SizeOfG2AffineUncompressed * toRead
+			read, err = io.ReadFull(dec.r, bufAll[:nbBytes])
 			dec.n += int64(read)
 			if err != nil {
-				return
+				return err
 			}
-			nbBytes := SizeOfG2AffineCompressed
-			// most significant byte contains metadata
-			if !isCompressed(buf[0]) {
-				nbBytes = SizeOfG2AffineUncompressed
-				// we read more.
-				read, err = io.ReadFull(dec.r, buf[SizeOfG2AffineCompressed:SizeOfG2AffineUncompressed])
-				dec.n += int64(read)
+			for i := 0; i < toRead; i++ {
+				_, err = (*t)[i+offset].setBytes(bufAll[i*SizeOfG2AffineUncompressed:(i+1)*SizeOfG2AffineUncompressed], false)
 				if err != nil {
-					return
-				}
-				_, err = (*t)[i].setBytes(buf[:nbBytes], false)
-				if err != nil {
-					return
-				}
-			} else {
-				compressed[i] = !((*t)[i].unsafeSetCompressedBytes(buf[:nbBytes]))
-			}
-		}
-		var nbErrs uint64
-		parallel.Execute(len(compressed), func(start, end int) {
-			for i := start; i < end; i++ {
-				if compressed[i] {
-					if err := (*t)[i].unsafeComputeY(dec.subGroupCheck); err != nil {
-						atomic.AddUint64(&nbErrs, 1)
-					}
-				} else if dec.subGroupCheck {
-					if !(*t)[i].IsInSubGroup() {
-						atomic.AddUint64(&nbErrs, 1)
-					}
+					return err
 				}
 			}
-		})
-		if nbErrs != 0 {
-			return errors.New("point decompression failed")
-		}
+			offset += toRead
 
+		}
 		return nil
 	default:
 		n := binary.Size(t)
